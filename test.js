1f8b7cdcb58a325c32fa874cb4ee71bcac02cdfa
sam0928 <sam813189@gmail.com>
2022-08-16 20:56:22 +0800

    1

---
+++ test.js
@@ -0,0 +1,46 @@
+var btn_ch_check = false;
+var btn_en_check = false;
+var btn_left_check = false;
+var btn_right_check = false;
+localStorage.clear()  // undefined   
+localStorage      //Storage {length: 0} 儲存資料
+
+function check(id){
+    if(id === "left"){
+        btn_left_check = true;
+    }else if(id === "right"){
+        btn_right_check = true;
+    }else if(id === "btn_ch"){
+        btn_ch_check = true;
+        btn_en_check = false;
+    }else{
+        btn_en_check = true;
+        btn_ch_check = false;
+    }
+
+    if(btn_en_check == true){
+        if(btn_left_check == true){
+            location.href = "./test.html";
+            // location.href = "../meal.en/monthly.html";
+            localStorage.setItem("state","內用") //儲存名字為name值為caibin的變數
+            localStorage // Storage{state: "in", length: 1} 讀取資料
+        }
+        if(btn_right_check == true){
+            location.href = "./test.html";
+            localStorage.setItem("state","外帶")
+            localStorage
+            // location.href = "../meal.en/monthly.html";
+        }
+    }else{
+        if(btn_left_check == true){
+            location.href = "../meal_ch/monthly.html";
+            localStorage.setItem("state","內用")
+            localStorage
+        }
+        if(btn_right_check == true){
+            location.href = "../meal_ch/monthly.html";
+            localStorage.setItem("state","外帶")
+            localStorage
+        }
+    }
+}
